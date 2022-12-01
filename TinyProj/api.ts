8e7bc64f0b3ceb006a885ed3ed25f489b2bfde1f
Ganbold KL Gantur <ganboldgantor14@gmail.com>
2022-12-01 20:07:30 +0800

    API

--- TinyProj/api.ts
+++ TinyProj/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const AXIOS = axios.create({
-  baseURL: "http://localhost:3030/"
+  baseURL: "https://react-native-unknownone76.vercel.app/"
 });
 
 export default AXIOS;
